6a79c89c57a7597bfb12f5e02afee864cab4e20b
Kaben Nanlohy <stochastic@hackamajig.com>
2012-01-06 18:17:42 -0500

    Moved CContour::SortEdges() from Contour.cpp to ContourStatics.cpp.
    Small refactor to help with unit test.

--- src/Contour.cpp
+++ src/Contour.cpp
@@ -222,68 +222,6 @@ const wxBitmap &CContour::GetIcon()
     return(GCPnts_AbscissaPoint::Length(curve));
 }
 
-/* static */ std::vector<TopoDS_Edge> CContour::SortEdges( const TopoDS_Wire & wire )
-{
-    std::vector<TopoDS_Edge> edges;
-
-	for(BRepTools_WireExplorer expEdge(TopoDS::Wire(wire)); expEdge.More(); expEdge.Next())
-	{
-	    edges.push_back( TopoDS_Edge(expEdge.Current()) );
-	} // End for
-
-	for (std::vector<TopoDS_Edge>::iterator l_itEdge = edges.begin(); l_itEdge != edges.end(); l_itEdge++)
-    {
-        if (l_itEdge == edges.begin())
-        {
-            // It's the first edge.  Find the edge whose endpoint is closest to gp_Pnt(0,0,0) so that
-            // the resutls of this sorting are consistent.  When we just use the first edge in the
-            // wire, we end up with different results every time.  We want consistency so that, if we
-            // use this Contour operation as a location for drilling a relief hole (one day), we want
-            // to be sure the machining will begin from a consistently known location.
-
-            std::vector<TopoDS_Edge>::iterator l_itStartingEdge = edges.begin();
-            gp_Pnt closest_point = GetStart(*l_itStartingEdge);
-            if (GetEnd(*l_itStartingEdge).Distance(gp_Pnt(0,0,0)) < closest_point.Distance(gp_Pnt(0,0,0)))
-            {
-                closest_point = GetEnd(*l_itStartingEdge);
-            }
-            for (std::vector<TopoDS_Edge>::iterator l_itCheck = edges.begin(); l_itCheck != edges.end(); l_itCheck++)
-            {
-                if (GetStart(*l_itCheck).Distance(gp_Pnt(0,0,0)) < closest_point.Distance(gp_Pnt(0,0,0)))
-                {
-                    closest_point = GetStart(*l_itCheck);
-                    l_itStartingEdge = l_itCheck;
-                }
-
-                if (GetEnd(*l_itCheck).Distance(gp_Pnt(0,0,0)) < closest_point.Distance(gp_Pnt(0,0,0)))
-                {
-                    closest_point = GetEnd(*l_itCheck);
-                    l_itStartingEdge = l_itCheck;
-                }
-            }
-
-            EdgeComparison compare( *l_itStartingEdge );
-            std::sort( edges.begin(), edges.end(), compare );
-        } // End if - then
-        else
-        {
-            // We've already begun.  Just sort based on the previous point's location.
-            std::vector<TopoDS_Edge>::iterator l_itNextEdge = l_itEdge;
-            l_itNextEdge++;
-
-            if (l_itNextEdge != edges.end())
-            {
-                EdgeComparison compare( *l_itEdge );
-                std::sort( l_itNextEdge, edges.end(), compare );
-            } // End if - then
-        } // End if - else
-    } // End for
-
-    return(edges);
-
-} // End SortEdges() method
-
-
 /**
     When we're starting a new sequence of edges, we want to run along the first edge
     so that we end up nearby to the next edge in the sorted sequence.  If we go in the
--- src/ContourStatics.cpp
+++ src/ContourStatics.cpp
@@ -1,5 +1,7 @@
 #include "Contour.h"
 #include <BRepAdaptor_Curve.hxx>
+#include <BRepTools_WireExplorer.hxx>
+#include <TopoDS.hxx>
 
 /* static */ gp_Pnt CContour::_GetStart(const TopoDS_Edge &edge)
 {
@@ -22,5 +24,66 @@
 
     return(PE);
 }
+
+/* static */ std::vector<TopoDS_Edge> CContour::SortEdges( const TopoDS_Wire & wire )
+{
+    std::vector<TopoDS_Edge> edges;
+
+	for(BRepTools_WireExplorer expEdge(TopoDS::Wire(wire)); expEdge.More(); expEdge.Next())
+	{
+	    edges.push_back( TopoDS_Edge(expEdge.Current()) );
+	} // End for
+
+	for (std::vector<TopoDS_Edge>::iterator l_itEdge = edges.begin(); l_itEdge != edges.end(); l_itEdge++)
+    {
+        if (l_itEdge == edges.begin())
+        {
+            // It's the first edge.  Find the edge whose endpoint is closest to gp_Pnt(0,0,0) so that
+            // the resutls of this sorting are consistent.  When we just use the first edge in the
+            // wire, we end up with different results every time.  We want consistency so that, if we
+            // use this Contour operation as a location for drilling a relief hole (one day), we want
+            // to be sure the machining will begin from a consistently known location.
+
+            std::vector<TopoDS_Edge>::iterator l_itStartingEdge = edges.begin();
+            gp_Pnt closest_point = _GetStart(*l_itStartingEdge);
+            if (_GetEnd(*l_itStartingEdge).Distance(gp_Pnt(0,0,0)) < closest_point.Distance(gp_Pnt(0,0,0)))
+            {
+                closest_point = _GetEnd(*l_itStartingEdge);
+            }
+            for (std::vector<TopoDS_Edge>::iterator l_itCheck = edges.begin(); l_itCheck != edges.end(); l_itCheck++)
+            {
+                if (_GetStart(*l_itCheck).Distance(gp_Pnt(0,0,0)) < closest_point.Distance(gp_Pnt(0,0,0)))
+                {
+                    closest_point = _GetStart(*l_itCheck);
+                    l_itStartingEdge = l_itCheck;
+                }
+
+                if (_GetEnd(*l_itCheck).Distance(gp_Pnt(0,0,0)) < closest_point.Distance(gp_Pnt(0,0,0)))
+                {
+                    closest_point = _GetEnd(*l_itCheck);
+                    l_itStartingEdge = l_itCheck;
+                }
+            }
+
+            EdgeComparison compare( *l_itStartingEdge );
+            std::sort( edges.begin(), edges.end(), compare );
+        } // End if - then
+        else
+        {
+            // We've already begun.  Just sort based on the previous point's location.
+            std::vector<TopoDS_Edge>::iterator l_itNextEdge = l_itEdge;
+            l_itNextEdge++;
+
+            if (l_itNextEdge != edges.end())
+            {
+                EdgeComparison compare( *l_itEdge );
+                std::sort( l_itNextEdge, edges.end(), compare );
+            } // End if - then
+        } // End if - else
+    } // End for
+
+    return(edges);
+
+} // End SortEdges() method
 
 
